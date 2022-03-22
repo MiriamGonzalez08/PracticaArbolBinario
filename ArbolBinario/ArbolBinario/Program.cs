280abb4b808fbe91f5c8f4eb42775fd8f35d9e0d
MiriamGonzalez08 <72994216+MiriamGonzalez08@users.noreply.github.com>
2022-03-22 12:11:27 -0600

    Add files via upload

---
+++ ArbolBinario/ArbolBinario/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ArbolBinario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número base");
+            int numBase = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese un limite de numeros");
+            int numLim = int.Parse(Console.ReadLine());
+            int[] ArbolBinario = new int[numLim];
+            int[] izquierda = new int[numLim];
+            int[] derecha = new int[numLim];
+        
+            for(int i = 0; i < ArbolBinario.Length; i++)
+            {
+                Console.WriteLine("ingrese un numero");
+                ArbolBinario[i] = int.Parse(Console.ReadLine());
+                if(ArbolBinario[i] < numBase)
+                {
+                    for (int j = 0; j < izquierda.Length; j++)
+                    {
+                        izquierda[j] = ArbolBinario[i];
+                    }
+                    Console.WriteLine("izquierda");
+                    Console.WriteLine(izquierda[i]);
+                    numBase = ArbolBinario[i];
+                }
+                    
+                else if(ArbolBinario[i] > numBase)
+                {
+                    for(int k = 0; k < derecha.Length; k++)
+                    {
+                        derecha[k] = ArbolBinario[i];                                              
+                    }
+                    Console.WriteLine("derecha");
+                    Console.WriteLine(derecha[i]);
+                    numBase = ArbolBinario[i];
+                }
+            }
+        }      
+    }
+}
+
